67bec601fa69be6f093ab2f99d70266b18c2ae97
punitha86 <punith.job@gmail.com>
2019-12-13 14:25:02 -0800

    adding htaccess and index in controller file

--- api/controllers/habits.php
+++ api/controllers/habits.php
@@ -1,1 +1,10 @@
- 
+
+<?php
+include_once __DIR__ . '/../models/post.php';
+header('Content-Type: application/json');
+
+if ($_REQUEST['action'] === 'INDEX') {
+  echo json_encode("indexPage");
+}
+
+ ?>
